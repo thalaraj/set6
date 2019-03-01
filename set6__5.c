fa661006cda43564fc1fc62dc4c456bb5173d715
thalaraj <47813910+thalaraj@users.noreply.github.com>
2019-02-28 17:04:45 -0800

    Create set6__5.c

---
+++ set6__5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d %d",&i,&j);
+    a=i*j;
+    if(a%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+    printf("odd");
+    }
+    return 0;
+}
